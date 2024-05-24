f39c6a0e858e1e20758bd0d1fa27636b264a313d
riskigalangsaputra <riskigalangsaputra@gmail.com>
2024-05-24 11:21:02 +0700

    connect to server eureka load balancer

--- src/main/java/id/kingra/otp_service/controller/OTPController.java
+++ src/main/java/id/kingra/otp_service/controller/OTPController.java
@@ -4,7 +4,9 @@ import id.kingra.otp_service.dto.OtpReqDto;
 import id.kingra.otp_service.service.OTPService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,12 +16,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class OTPController {
 
     private final OTPService otpService;
+    private final Environment environment;
 
     @PostMapping("/otp/request")
     public ResponseEntity<?> requestOTP(@RequestBody OtpReqDto request) {
         log.debug("Request OTP : {}", request);
         otpService.requestOTP(request);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/test-load-balancer")
+    public String testLoadBalancer() {
+        String port = environment.getProperty("local.server.port");
+        log.debug("Execute Port : {}", port);
+        return port;
     }
 }
 
